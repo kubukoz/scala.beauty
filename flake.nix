9f5bc7cdb6571d2be77c7d0cb58d48c2b4eb32e8
Jakub Kozłowski <kubukoz@gmail.com>
2024-06-07 01:07:37 +0200

    Add nix shell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.yarn
+            pkgs.sbt
+          ];
+        };
+      }
+    );
+}
