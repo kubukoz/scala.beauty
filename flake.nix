6a6ce7f35d36cd45109a041575eaeb4ba587d5a3
Jakub Kozłowski <kubukoz@gmail.com>
2024-07-05 18:49:47 +0200

    wip dagger

--- flake.nix
+++ flake.nix
@@ -11,12 +11,31 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        dagger = pkgs.stdenv.mkDerivation {
+          name = "dagger";
+          src = builtins.fetchurl {
+            url = "https://github.com/dagger/dagger/releases/download/v0.11.9/dagger_v0.11.9_darwin_arm64.tar.gz";
+            sha256 = "19b32f83bwri49vipi4kkrgn9lxi6bbikqxjhcl4p8vk3ivvm38c";
+          };
+          dontUnpack = true;
+
+          buildPhase = ''
+            tar xzf $src
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp dagger $out/bin
+          '';
+        };
       in
       {
         devShell = pkgs.mkShell {
           buildInputs = [
             pkgs.yarn
             pkgs.sbt
+            dagger
           ];
         };
       }
