0760a685242b41addc3ddd19477bc3523ced5a0a
Jakub Kozłowski <kubukoz@gmail.com>
2024-07-06 19:36:42 +0200

    Add previous/next page buttons

--- modules/frontend/src/main/scala/scalabeauty/frontend/Pagination.scala
+++ modules/frontend/src/main/scala/scalabeauty/frontend/Pagination.scala
@@ -12,6 +12,8 @@ object Pagination {
   enum Block {
     case Ellipsis
     case PageNumber(number: Long, current: Boolean)
+    case Previous(page: Option[Long])
+    case Next(page: Option[Long])
   }
 
   object Block {
@@ -29,26 +31,22 @@ object Pagination {
   def getPaginationBlocks(current: Long, total: Long): List[Block] = {
     import Block._
 
+    val firstPage    = 0L
+    val previousPage = current - 1
+    val nextPage     = current + 1
+    val lastPage     = total - 1L
+
     // first and last page always visible
     // current page and 1 page before / after always visible
     // ellipsis in between if there's a gap.
     // if total pages <= 5, all pages visible
     val numbersToShow = List
       .concat(
-        // first page
-        List(0L),
-
-        // previous
-        List(current - 1).filter(_ >= 0),
-
-        // current
+        List(firstPage),
+        List(previousPage).filter(_ >= 0),
         List(current).filterNot(_ == 0).filterNot(_ == total),
-
-        // next
-        List(current + 1).filter(_ < total),
-
-        // last
-        List(total - 1),
+        List(nextPage).filter(_ < total),
+        List(lastPage),
       )
       .distinct
 
@@ -62,8 +60,10 @@ object Pagination {
       }
       .map {
         case PageNumber(number, _) => PageNumber(number, current = number == current)
-        case Ellipsis              => Ellipsis
+        case other                 => other
       }
+      .prepended(Block.Previous(previousPage.some.filter(_ >= 0)))
+      .appended(Block.Next(nextPage.some.filter(_ <= (total - 1))))
   }
 
   def view(blocks: List[Block]): Html[Nothing] =
@@ -73,17 +73,51 @@ object Pagination {
     )(
       ul(className := "pagination-list")(
         blocks.map {
+          case Block.Previous(page: Option[Long]) =>
+            page match {
+              case Some(page) =>
+                li(
+                  a(
+                    className := "pagination-previous is-family-monospace",
+                    href      := s"?page=$page",
+                  )("--")
+                )
+              case None =>
+                li(
+                  span(
+                    className := "pagination-previous is-family-monospace is-disabled"
+                  )("--")
+                )
+            }
+
+          case Block.Next(page: Option[Long]) =>
+            page match {
+              case Some(page) =>
+                li(
+                  a(
+                    className := "pagination-next is-family-monospace",
+                    href      := s"?page=$page",
+                  )("++")
+                )
+              case None =>
+                li(
+                  span(
+                    className := "pagination-next is-family-monospace is-disabled"
+                  )("++")
+                )
+            }
+
           case Block.Ellipsis => li(span(className := "pagination-ellipsis")("…"))
           case Block.PageNumber(number, current) if current =>
             li(
               span(
-                className := "pagination-link is-current"
+                className := "pagination-link is-current is-family-monospace"
               )(number.toString)
             )
           case Block.PageNumber(number, _) =>
             li(
               a(
-                className := s"pagination-link",
+                className := s"pagination-link is-family-monospace",
                 href      := s"?page=$number",
               )(number.toString)
             )
--- modules/frontend/src/test/scala/scalabeauty/frontend/test/PaginationTests.scala
+++ modules/frontend/src/test/scala/scalabeauty/frontend/test/PaginationTests.scala
@@ -7,7 +7,7 @@ import weaver.*
 object PaginationTests extends SimpleIOSuite {
   import Pagination.Block.*
 
-  private def paginationTest(current: Long, total: Long)(expected: List[Block]): Unit =
+  private def paginationTest(current: Long, total: Long)(expected: List[Block])(using SourceLocation): Unit =
     pureTest(s"$current. page of $total") {
       assert.same(
         expected,
@@ -20,66 +20,81 @@ object PaginationTests extends SimpleIOSuite {
 
   paginationTest(0, 1)(
     List(
-      PageNumber(0).asCurrent
+      Previous(None),
+      PageNumber(0).asCurrent,
+      Next(None),
     )
   )
 
   paginationTest(0, 2)(
     List(
+      Previous(None),
       PageNumber(0).asCurrent,
       PageNumber(1),
+      Next(Some(1)),
     )
   )
 
   paginationTest(0, 3)(
     List(
+      Previous(None),
       PageNumber(0).asCurrent,
       PageNumber(1),
       PageNumber(2),
+      Next(Some(1)),
     )
   )
 
   paginationTest(0, 4)(
     List(
+      Previous(None),
       PageNumber(0).asCurrent,
       PageNumber(1),
       Ellipsis,
       PageNumber(3),
+      Next(Some(1)),
     )
   )
 
   paginationTest(0, 10)(
     List(
+      Previous(None),
       PageNumber(0).asCurrent,
       PageNumber(1),
       Ellipsis,
       PageNumber(9),
+      Next(Some(1)),
     )
   )
 
   paginationTest(1, 10)(
     List(
+      Previous(Some(0)),
       PageNumber(0),
       PageNumber(1).asCurrent,
       PageNumber(2),
       Ellipsis,
       PageNumber(9),
+      Next(Some(2)),
     )
   )
 
   paginationTest(2, 10)(
     List(
+      Previous(Some(1)),
       PageNumber(0),
       PageNumber(1),
       PageNumber(2).asCurrent,
       PageNumber(3),
       Ellipsis,
       PageNumber(9),
+      Next(Some(3)),
     )
   )
 
   paginationTest(3, 10)(
     List(
+      Previous(Some(2)),
       PageNumber(0),
       Ellipsis,
       PageNumber(2),
@@ -87,36 +102,43 @@ object PaginationTests extends SimpleIOSuite {
       PageNumber(4),
       Ellipsis,
       PageNumber(9),
+      Next(Some(4)),
     )
   )
 
   paginationTest(7, 10)(
     List(
+      Previous(Some(6)),
       PageNumber(0),
       Ellipsis,
       PageNumber(6),
       PageNumber(7).asCurrent,
       PageNumber(8),
       PageNumber(9),
+      Next(Some(8)),
     )
   )
 
   paginationTest(8, 10)(
     List(
+      Previous(Some(7)),
       PageNumber(0),
       Ellipsis,
       PageNumber(7),
       PageNumber(8).asCurrent,
       PageNumber(9),
+      Next(Some(9)),
     )
   )
 
   paginationTest(9, 10)(
     List(
+      Previous(Some(8)),
       PageNumber(0),
       Ellipsis,
       PageNumber(8),
       PageNumber(9).asCurrent,
+      Next(None),
     )
   )
 }
