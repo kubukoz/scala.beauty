8309623fb6f688ac55ea8d3c640db641828bf386
Jakub Kozłowski <kubukoz@gmail.com>
2024-07-12 02:09:44 +0200

    TRY AWS THIS TOTALLY DOES NOT WORK

--- modules/infra/Main.scala
+++ modules/infra/Main.scala
@@ -1,46 +1,90 @@
 import besom.*
-import besom.api.heroku.*
-import besom.api.heroku.addon.Addon
-import besom.api.heroku.addon.AddonArgs
-import besom.api.heroku.app.App
-import besom.api.heroku.app.AppArgs
-import besom.api.heroku.build.inputs.BuildSourceArgs
-import besom.api.heroku.build.Build
-import besom.api.heroku.build.BuildArgs
+import besom.api.aws
+import besom.api.aws.ec2
+import besom.api.aws.ec2.Vpc
+import besom.api.aws.ec2.VpcArgs
+import besom.api.aws.ecr
+import besom.api.aws.ecr.Repository
+import besom.api.aws.ecs
+import besom.api.aws.ecs.inputs.ServiceLoadBalancerArgs
+import besom.api.aws.ecs.inputs.TaskDefinitionRuntimePlatformArgs
+import besom.api.awsx.ecr as ecrx
+import besom.api.awsx.ecs.inputs.FargateServiceTaskDefinitionArgs
+import besom.api.awsx.ecs.inputs.TaskDefinitionContainerDefinitionArgs
+import besom.api.awsx.ecs.FargateTaskDefinition
+import besom.api.awsx.ecs.FargateTaskDefinitionArgs
+import besom.api.awsx.ecs as ecsx
+import besom.api.awsx.lb as lbx
 
 @main def main = Pulumi.run {
 
-  val app = App(
-    name = "scala-beauty",
-    args = AppArgs(
-      region = "eu",
-      stack = "container",
+  inline def n(inline s: String) = s"scala-beauty-$s"
+
+  // val vpc = ec2.Vpc(
+  //   n("vpc"),
+  //   ec2.VpcArgs(
+  //     cidrBlock = "10.0.0.0/16"
+  //   ),
+  // )
+
+  val lb = lbx.ApplicationLoadBalancer(n("lb"))
+
+  val cluster = ecs.Cluster(
+    n("cluster"),
+    ecs.ClusterArgs(
     ),
   )
 
-  val postgresAddon = Addon(
-    name = "postgres",
-    args = AddonArgs(
-      appId = app.id,
-      plan = "heroku-postgresql:essential-0",
+  val repo = ecr.Repository(n("repo"), ecr.RepositoryArgs())
+
+  val image = ecrx.Image(
+    n("image"),
+    ecrx.ImageArgs(
+      repositoryUrl = repo.repositoryUrl,
+      context = "build",
+      dockerfile = "build/Dockerfile",
+      platform = "linux/arm64",
     ),
   )
 
-  val build = Build(
-    "build",
-    BuildArgs(
-      appId = app.id,
-      source = BuildSourceArgs(
-        // separate directory so that its checksum doesn't get affected when we change the build script
-        path = "build"
+  val service = ecsx.FargateService(
+    "service",
+    ecsx.FargateServiceArgs(
+      cluster = cluster.arn,
+      assignPublicIp = true,
+      desiredCount = 1,
+      taskDefinitionArgs = FargateServiceTaskDefinitionArgs(
+        container = TaskDefinitionContainerDefinitionArgs(
+          name = n("container"),
+          image = image.imageUri,
+          cpu = 512,
+          memory = 1024,
+          essential = true,
+          portMappings = List(
+            // ecsx.inputs.TaskDefinitionPortMappingArgs(
+            // containerPort = 8080
+            // hostPort = 8080,
+            // protocol = "tcp",
+            // https://github.com/VirtusLab/besom/issues/398
+            // targetGroup = lb.defaultTargetGroup,
+            // )
+          ),
+        ),
+        runtimePlatform = TaskDefinitionRuntimePlatformArgs(
+          cpuArchitecture = "ARM64"
+        ),
+      ),
+      loadBalancers = List(
+        ServiceLoadBalancerArgs(
+          containerName = n("container"),
+          containerPort = 80,
+          targetGroupArn = lb.defaultTargetGroup.arn,
+        )
       ),
     ),
-    opts(dependsOn = postgresAddon),
   )
 
-  Stack(postgresAddon, build).exports(
-    appName = app.name,
-    url = app.webUrl,
-    postgresVars = postgresAddon.configVarValues,
+  Stack(service).exports(
+    appUrl = p"http://${lb.loadBalancer.dnsName}"
   )
 }
--- modules/infra/project.scala
+++ modules/infra/project.scala
@@ -1,4 +1,6 @@
-//> using scala "3.4.2"
+//> using scala "3.3.1"
 //> using options -Wunused:all -Wvalue-discard -Wnonunit-statement
 //> using dep "org.virtuslab::besom-core:0.3.2"
-//> using dep "org.virtuslab::besom-heroku:1.0.3-core.0.3"
+//> using dep "org.virtuslab::besom-aws:6.39.0-core.0.3"
+//> using dep "org.virtuslab::besom-awsx:2.11.0-core.0.3"
+//> using plugin "org.virtuslab::besom-compiler-plugin:0.3.2"
